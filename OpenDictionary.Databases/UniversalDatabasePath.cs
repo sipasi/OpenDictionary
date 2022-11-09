540649e11e8f473209cc3202833ff990b161af8e
sipasi <tanksasha77@gmail.com>
2022-11-09 12:19:18 +0200

    Made database path to set manually or default

--- OpenDictionary.Databases/UniversalDatabasePath.cs
+++ OpenDictionary.Databases/UniversalDatabasePath.cs
@@ -1,9 +1,11 @@
-﻿
-using System;
-
-namespace OpenDictionary.Databases;
+﻿namespace OpenDictionary.Databases;
 
 public sealed class UniversalDatabasePath : IDatabasePath
 {
-    public string Path { get; } = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "database.db");
+    public string Path { get; }
+
+    public UniversalDatabasePath(string name = "database")
+    {
+        Path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), $"{name}.db");
+    }
 }
