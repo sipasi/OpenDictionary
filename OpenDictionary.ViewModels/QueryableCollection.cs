0b95804d283daf7596ff489a1cb796361ab9971c
sipasi <tanksasha77@gmail.com>
2022-11-11 12:26:27 +0200

    Made Collection more flexible

--- OpenDictionary.ViewModels/QueryableCollection.cs
+++ OpenDictionary.ViewModels/QueryableCollection.cs
@@ -1,20 +1,28 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 using OpenDictionary.Collections.Storages;
 
 namespace OpenDictionary.ViewModels;
 
-public delegate IQueryable<T> QueryFactory<T>(IQueryable<T> query);
+public delegate IQueryable<TOut> QueryFactory<TIn, TOut>(IQueryable<TIn> query);
 
-public class QueryableCollection<T> : CollectionViewModel<T>
+public class QueryableCollection<T> : QueryableCollection<T, T>
 {
-    private readonly IStorage<T> storage;
+    public QueryableCollection(IStorage<T> storage, QueryFactory<T, T> query, Func<T?, Task> tappedCommand)
+        : base(storage, query, tappedCommand) { }
+}
 
-    public QueryFactory<T> Query { get; }
+public class QueryableCollection<TStorage, TElement> : CollectionViewModel<TElement>
+{
+    private readonly IStorage<TStorage> storage;
 
-    public QueryableCollection(IStorage<T> storage, QueryFactory<T> query, Func<T?, Task> tappedCommand)
+    public QueryFactory<TStorage, TElement> Query { get; }
+
+    public QueryableCollection(IStorage<TStorage> storage, QueryFactory<TStorage, TElement> query, Func<TElement?, Task> tappedCommand)
     {
         this.storage = storage;
 
@@ -29,11 +37,20 @@ public class QueryableCollection<T> : CollectionViewModel<T>
     {
         IsBusy = true;
 
-        IQueryable<T> query = storage.Query();
+        IQueryable<TElement> query = Query.Invoke(storage.Query());
 
-        query = Query.Invoke(query);
+        TElement[]? items = default;
 
-        var items = await Task.Run(query.ToArray);
+        try
+        {
+            items = await Task.Run(query.ToArray);
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e.Message);
+
+            return;
+        }
 
         Collection.Clear();
 
