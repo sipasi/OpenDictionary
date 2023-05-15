0257d745d81588551efc2a9d8a4cfae2b198fa0b
sipasi <tanksasha77@gmail.com>
2023-05-15 12:33:35 +0300

    feat: implement IAppOrBrowserLauncher

---
+++ OpenDictionary.MauiProject/Services/ExternalAppTranslation/AppOrBrowserLauncher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Maui.ApplicationModel;
+
+using OpenDictionary.ExternalAppTranslation;
+
+namespace OpenDictionary.Services.ExternalAppTranslation;
+
+internal sealed class AppOrBrowserLauncher(ILauncher launcher) : IAppOrBrowserLauncher
+{
+    public async ValueTask OpenAsync(Uri uri)
+    {
+        await launcher.OpenAsync(uri);
+    }
+}
