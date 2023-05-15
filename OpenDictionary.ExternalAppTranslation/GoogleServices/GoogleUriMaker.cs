2c1246230e77b1d62f3ce7a679bb2f4c74499c1f
sipasi <tanksasha77@gmail.com>
2023-05-15 12:30:15 +0300

    feat: add GoogleUriMaker for URL generation in Google translation

---
+++ OpenDictionary.ExternalAppTranslation/GoogleServices/GoogleUriMaker.cs
@@ -0,0 +1,18 @@
+﻿namespace OpenDictionary.ExternalAppTranslation.GoogleServices;
+
+public sealed class GoogleUriMaker : ITranslatorUrlMaker
+{
+    public Uri Make(string text, TranslationOptions options)
+    {
+        string escaped = Uri.EscapeDataString(text);
+
+        string result = string.Concat(
+            "https://translate.google.com/",
+            "?sl=", options.From.TwoLetterISOLanguageName,
+            "&tl=", options.To.TwoLetterISOLanguageName,
+            "&text=", escaped,
+            "&op=translate");
+
+        return new(result);
+    }
+}
