4fe8748d361f435e36f6825ccf449d01bcff7af4
sipasi <tanksasha77@gmail.com>
2023-05-15 12:34:23 +0300

    feat: implement INavigationService interface

---
+++ OpenDictionary.MauiProject/Services/Navigations/NavigationService.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.Maui.Controls;
+
+using OpenDictionary.MauiProject;
+
+namespace OpenDictionary.Services.Navigations;
+
+internal sealed class NavigationService : INavigationService
+{
+    public NavigationService()
+    {
+        Shell.Current.Navigating += Navigating;
+    }
+
+    public Task GoToAsync(string route) => Shell.Current.GoToAsync(route);
+    public Task GoToAsync(string route, string parameter, string value) => Shell.Current.GoToAsync($"{route}?{parameter}={value}");
+    public Task GoBackAsync() => Shell.Current.GoToAsync("..");
+
+    private void Navigating(object? sender, ShellNavigatingEventArgs e)
+    {
+        if (Shell.Current is not AppShell shell)
+        {
+            return;
+        }
+
+        if (e is { CanCancel: false, Source: not ShellNavigationSource.Pop })
+        {
+            return;
+        }
+
+        if (shell.CurrentPage is Views.Pages.SettingsPage)
+        {
+            e.Cancel();
+
+            shell.SetListPage();
+        }
+    }
+}
