bd4cb47441d29d0c4c43438b6eee7456a7896233
sipasi <tanksasha77@gmail.com>
2023-05-15 12:31:48 +0300

    feat: add GoogleExternalTranslator for Google Translate integration

---
+++ OpenDictionary.ExternalAppTranslation/GoogleServices/GoogleExternalTranslator.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace OpenDictionary.ExternalAppTranslation.GoogleServices;
+
+public sealed class GoogleExternalTranslator(IAppOrBrowserLauncher launcher, ITranslatorUrlMaker urlMaker) : IExternalTranslator
+{
+    public async ValueTask Open(string text, TranslationOptions options)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return;
+        }
+
+        try
+        {
+            Uri uri = urlMaker.Make(text, options);
+
+            await launcher.OpenAsync(uri);
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e.Message);
+        }
+    }
+}
