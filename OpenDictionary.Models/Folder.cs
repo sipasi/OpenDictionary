4cec01f42827cedfb1cbc86c08c2283099604b4c
sipasi <tanksasha77@gmail.com>
2023-06-06 12:27:06 +0300

    feat: add folder that can contains WordGroup or other folder

---
+++ OpenDictionary.Models/Folder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenDictionary.Models;
+
+public class Folder : IEntity
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public List<WordGroup> Groups { get; set; } = null!;
+
+    public Guid? ParentId { get; set; }
+    public Folder? Parent { get; set; }
+
+    public List<Folder> Subfolders { get; set; } = null!;
+}
